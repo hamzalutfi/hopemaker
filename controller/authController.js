ea6018fb8269f6fa7800ade5811856e02e2aaed8
hamzalutfi <hamza.lutfi68@gmail.com>
2024-12-03 17:29:30 +0100

    first edit

--- controller/authController.js
+++ controller/authController.js
@@ -41,5 +41,95 @@ exports.login=async (req,res)=>{
         res.status(500).send(err);
     }
 }
+exports.forgotPassword = async (req, res) => {
+    try {
+      const { email } = req.body;
+      const curruntUser = await user.findOne({ email });
+      if (!curruntUser) {
+        res.status(500).send("there is no user found");
+        return;
+      }
+      const userToken = curruntUser.createPasswordRestToken();
+      sendResetPasswordEmail({
+        email:curruntUser.email,
+        restToken: userToken,
+      });
+      curruntUser.save({ validateBeforeSave: false });
+      res.send("done");
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  };
+  exports.resendactivationCode=async(req,res)=>{
+    try {
+      const {email}=req.body;
+      const currentUser= await user.findOne({email});
+      if(!currentUser){
+        res.status(500).send("there is no user found");
+        return; 
+      }
+      const userToken = currentUser.createActivationToken(); // take the token from method in user model
+      await sendActivationEmail({
+        // make activate for email
+        email: currentUser.email,
+        token: userToken,
+        name: currentUser.userName,
+      });
+      currentUser.save({ validateBeforeSave: false });
+      console.log(userToken);
+      res.send("done");
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  }
 
-
+  exports.activateAccount = async (req, res) => {
+    try {
+      const { email, token } = req.body;
+      const currUser = await user.findOne({ email });
+  
+      if (!currUser) {
+        res.status(500).send("the user does not exist");
+        return;
+      }
+  
+      const tokenHashed = crypto.createHash("sha256").update(token).digest("hex");
+      if (currUser.activationToken !== tokenHashed) {
+        res.status(500).send("the token does not match");
+        return;
+      }
+      currUser.status = "active";
+      currUser.save({ validateBeforeSave: false });
+      res.send("done");
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  };
+  exports.resetPassword = async (req, res) => {
+    try {
+      const { email, token, password, confirmPassword } = req.body;
+      const currrUser = await user.findOne({ email }).select("+password");
+      if (!currrUser) {
+        res.status(500).send("there is no user found");
+        return;
+      }
+      const newToken=crypto
+      .createHash("sha256")
+      .update(token)
+      .digest("hex");
+      if(currrUser.passwordResetToken!==newToken){
+        res.status(500).send('token not match')
+        return;
+      }
+    
+      currrUser.password = password;
+      currrUser.confirmPassword = confirmPassword;
+  
+      currrUser.save();
+      res.send(currrUser.password)
+    }
+     catch (e) {
+      res.status(500).send(e);
+    }
+    
+  };
--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,9 @@ const authController = require("../controller/authController");
 
 
 router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.post("/forgotpassword", authController.forgotPassword);
+router.post("/activate", authController.activateAccount);
+router.post("/resendactivate", authController.resendactivationCode);
+router.patch("/resetpassword", authController.resetPassword);
 module.exports = router;
